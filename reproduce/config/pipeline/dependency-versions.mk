a1aedecd601788bc7a3f34848fdfa6531fb873b6
Mohammad Akhlaghi <mohammad@akhlaghi.org>
2019-03-29 20:01:18 +0000

    Added copyright information in dependency-versions.mk
    To help make it easier to re-use (like the rest of the "large" files).

--- reproduce/config/pipeline/dependency-versions.mk
+++ reproduce/config/pipeline/dependency-versions.mk
@@ -1,4 +1,23 @@
-# Versions of the various dependnecies
+# Versions of the various dependencies
+#
+# Original author:
+#     Mohammad Akhlaghi <mohammad@akhlaghi.org>
+# Contributing author(s):
+#     Raul Infante-Sainz <infantesainz@gmail.com>
+# Copyright (C) 2018-2019, Your Name.
+#
+# This Makefile is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# This Makefile is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# A copy of the GNU General Public License is available at
+# <http://www.gnu.org/licenses/>.
 
 # Programs
 bash-version               = 5.0
