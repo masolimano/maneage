389d6e2862a0ecf0a6d849a915b02a17b304e45a
Raul Infante-Sainz <infantesainz@gmail.com>
2019-04-29 10:56:26 +0100

    Modified copyright information of some files
    With this commit, the copyright information of some files have been
    modify in order to include the work of Raul Infante-Sainz.

--- reproduce/software/make/high-level.mk
+++ reproduce/software/make/high-level.mk
@@ -9,6 +9,7 @@
 # ------------------------------------------------------------------------
 #
 # Copyright (C) 2018-2019 Mohammad Akhlaghi <mohammad@akhlaghi.org>
+# Copyright (C) 2019 Raul Infante-Sainz <infantesainz@gmail.com>
 #
 # This Makefile is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
