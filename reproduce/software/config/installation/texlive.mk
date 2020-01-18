3c05235a86bb19e74a4ffb656b909928bdfc2126
Mohammad Akhlaghi <mohammad@akhlaghi.org>
2020-01-18 00:48:58 +0000

    Added mweights as a TeXLive package
    After a new rebuild of the project, I noticed that we now need to also
    build the `mweights' package.

--- reproduce/software/config/installation/texlive.mk
+++ reproduce/software/config/installation/texlive.mk
@@ -20,4 +20,5 @@ texlive-packages = tex fancyhdr ec newtx fontaxes xkeyval etoolbox xcolor \
                    setspace caption footmisc datetime fmtcount titlesec \
                    preprint ulem biblatex biber logreq pgf pgfplots fp \
                    courier tex-gyre txfonts times csquotes kastrup \
-                   trimspaces pdftexcmds pdfescape letltxmacro bitset
+                   trimspaces pdftexcmds pdfescape letltxmacro bitset \
+                   mweights
